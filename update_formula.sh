20d9ca405a066d98a7d5525233804e4ca4fc08f5
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-01-23 16:18:19 -0700

    Updates the klotho formula template and script to support multiple variants

--- update_formula.sh
+++ update_formula.sh
@@ -8,8 +8,9 @@ VERSION=$(echo "${VERSION}" | cut -d "v" -f2)
 mkdir -p Formula
 cp klotho.rb.tmpl Formula/klotho.rb
 
-sed -i "s/{{BINARY_BASE_URL}}/${BINARY_BASE_URL}/g" Formula/klotho.rb
-sed -i "s/{{VERSION}}/${VERSION}/g" Formula/klotho.rb
-sed -i "s/{{MACOS_ARM_SHA256}}/$(shasum -a 256 -b klotho_darwin_arm64 | cut -d " " -f1)/" Formula/klotho.rb
-sed -i "s/{{MACOS_INTEL_SHA256}}/$(shasum -a 256 -b klotho_darwin_amd64 | cut -d " " -f1)/" Formula/klotho.rb
-sed -i "s/{{LINUX_INTEL_SHA256}}/$(shasum -a 256 -b klotho_linux_amd64 | cut -d " " -f1)/" Formula/klotho.rb
+sed -i "s/{{FORMULA_NAME}}/${FORMULA_NAME}/g" Formula/${OUTPUT_FILE_NAME}
+sed -i "s/{{BINARY_BASE_URL}}/${BINARY_BASE_URL}/g" Formula/${OUTPUT_FILE_NAME}
+sed -i "s/{{VERSION}}/${VERSION}/g" Formula/${OUTPUT_FILE_NAME}
+sed -i "s/{{MACOS_ARM_SHA256}}/$(shasum -a 256 -b klotho_darwin_arm64 | cut -d " " -f1)/" Formula/${OUTPUT_FILE_NAME}
+sed -i "s/{{MACOS_INTEL_SHA256}}/$(shasum -a 256 -b klotho_darwin_amd64 | cut -d " " -f1)/" Formula/${OUTPUT_FILE_NAME}
+sed -i "s/{{LINUX_INTEL_SHA256}}/$(shasum -a 256 -b klotho_linux_amd64 | cut -d " " -f1)/" Formula/${OUTPUT_FILE_NAME}
