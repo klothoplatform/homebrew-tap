716541494c4f13fb7fedd93647dbf028c96cc259
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-01-26 15:24:43 -0700

    Moves git add to end of script

--- update_formula.sh
+++ update_formula.sh
@@ -8,7 +8,6 @@ OUTPUT_PATH="Formula/${OUTPUT_FILE_NAME}"
 
 mkdir -p Formula
 cp klotho.rb.tmpl $OUTPUT_PATH
-git add $OUTPUT_PATH
 
 sed -i "s/{{FORMULA_NAME}}/${FORMULA_NAME}/g" $OUTPUT_PATH
 sed -i "s/{{BINARY_BASE_URL}}/${BINARY_BASE_URL}/g" $OUTPUT_PATH
@@ -16,3 +15,5 @@ sed -i "s/{{VERSION}}/${VERSION}/g" $OUTPUT_PATH
 sed -i "s/{{MACOS_ARM_SHA256}}/$(shasum -a 256 -b klotho_darwin_arm64 | cut -d " " -f1)/" $OUTPUT_PATH
 sed -i "s/{{MACOS_INTEL_SHA256}}/$(shasum -a 256 -b klotho_darwin_amd64 | cut -d " " -f1)/" $OUTPUT_PATH
 sed -i "s/{{LINUX_INTEL_SHA256}}/$(shasum -a 256 -b klotho_linux_amd64 | cut -d " " -f1)/" $OUTPUT_PATH
+
+git add $OUTPUT_PATH
