c5884e40f3ae39a570a9435bac8b502e0f43c8fa
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-01-26 15:09:34 -0700

    Adds formula file to the repo if it's new

--- update_formula.sh
+++ update_formula.sh
@@ -4,13 +4,17 @@ set -e -u
 
 BINARY_BASE_URL=$(echo ${BINARY_BASE_URL} | sed 's/\//\\\//g')
 VERSION=$(echo "${VERSION}" | cut -d "v" -f2)
+OUTPUT_PATH="Formula/${OUTPUT_FILE_NAME}"
+
 
 mkdir -p Formula
-cp klotho.rb.tmpl "Formula/${OUTPUT_FILE_NAME}"
+cp klotho.rb.tmpl $OUTPUT_PATH
 
-sed -i "s/{{FORMULA_NAME}}/${FORMULA_NAME}/g" Formula/${OUTPUT_FILE_NAME}
-sed -i "s/{{BINARY_BASE_URL}}/${BINARY_BASE_URL}/g" Formula/${OUTPUT_FILE_NAME}
-sed -i "s/{{VERSION}}/${VERSION}/g" Formula/${OUTPUT_FILE_NAME}
-sed -i "s/{{MACOS_ARM_SHA256}}/$(shasum -a 256 -b klotho_darwin_arm64 | cut -d " " -f1)/" Formula/${OUTPUT_FILE_NAME}
-sed -i "s/{{MACOS_INTEL_SHA256}}/$(shasum -a 256 -b klotho_darwin_amd64 | cut -d " " -f1)/" Formula/${OUTPUT_FILE_NAME}
-sed -i "s/{{LINUX_INTEL_SHA256}}/$(shasum -a 256 -b klotho_linux_amd64 | cut -d " " -f1)/" Formula/${OUTPUT_FILE_NAME}
+git add $OUTPUT_PATH
+
+sed -i "s/{{FORMULA_NAME}}/${FORMULA_NAME}/g" $OUTPUT_PATH
+sed -i "s/{{BINARY_BASE_URL}}/${BINARY_BASE_URL}/g" $OUTPUT_PATH
+sed -i "s/{{VERSION}}/${VERSION}/g" $OUTPUT_PATH
+sed -i "s/{{MACOS_ARM_SHA256}}/$(shasum -a 256 -b klotho_darwin_arm64 | cut -d " " -f1)/" $OUTPUT_PATH
+sed -i "s/{{MACOS_INTEL_SHA256}}/$(shasum -a 256 -b klotho_darwin_amd64 | cut -d " " -f1)/" $OUTPUT_PATH
+sed -i "s/{{LINUX_INTEL_SHA256}}/$(shasum -a 256 -b klotho_linux_amd64 | cut -d " " -f1)/" $OUTPUT_PATH
